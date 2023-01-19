bfc54e554c953bcb34e97a8b5c22b4aa1ec69afd
custibor99 <cus.tibor@outlook.com>
2023-01-19 17:49:18 +0100

    Migrating to postges and creating initial data.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,8 @@ insert into software(id, name) VALUES (3, 'qsar toolbox')
 insert into software(id, name) VALUES (4, 'acd perceptra')
 insert into software(id, name) VALUES (5, 'opera')
 insert into software(id, name) VALUES (6, 'test')
+
+insert into endpoint(id, name) VALUES(1,'sperm effect')
+insert into endpoint(id, name) VALUES(2,'reproductive toxicity')
+insert into endpoint(id, name) VALUES(3,'mutagenicity')
+insert into endpoint(id, name) VALUES(4,'carcinogenicity')
